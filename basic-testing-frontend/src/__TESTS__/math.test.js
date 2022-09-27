76c721de0d56733dfa56588bb69455490467ba6d
Marknjo <marknjo7@gmail.com>
2022-09-27 15:54:40 +0300

    test(frontend): add NAN test

--- basic-testing-frontend/src/__TESTS__/math.test.js
+++ basic-testing-frontend/src/__TESTS__/math.test.js
@@ -14,6 +14,14 @@ it("should summarize all number values in an array", () => {
   expect(result).toBe(sumValues);
 });
 
+it("should yield a NAN if one of the supplied values in not a number", () => {
+  const values = [1, "number"];
+
+  const result = add(values);
+
+  expect(result).toBeNaN();
+});
+
 it.todo("should throw an error if supplied values is not an array");
 it.todo("should accept only array of number");
 it.todo("should throw if supplied array is not array of numbers");
--- basic-testing-frontend/src/math.js
+++ basic-testing-frontend/src/math.js
@@ -2,6 +2,9 @@ export function add(numbers) {
   let sum = 0;
 
   for (const number of numbers) {
+    if (isNaN(number)) {
+      return NaN;
+    }
     sum += number;
   }
   return sum;
