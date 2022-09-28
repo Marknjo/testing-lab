5ec73c264fa889cc10571fb028b168342440457b
Marknjo <marknjo7@gmail.com>
2022-09-28 14:04:53 +0300

    test(frontend): test transformToNumber() returns a number

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,6 +1,14 @@
 import { it, expect } from "vitest";
+import { transformToNumber } from "./numbers";
 
-it.todo("should convert a number to a string");
+it.todo("should convert a number to a string", () => {
+  const num = "1";
+
+  const result = transformToNumber(num);
+
+  expect(result).toBe("number");
+});
+
 it.todo("should throw if a argument value is not a number of a string");
 it.todo("should throw if an empty argument is provided");
 it.todo("should ignore more than one argument");
