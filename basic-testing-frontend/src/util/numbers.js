1fa85863b6f1ca1d6b1535b2801b543640c01564
Marknjo <marknjo7@gmail.com>
2022-09-28 14:18:34 +0300

    test(frontend): test transformToNumber() throws if invalid arguments are provided

--- basic-testing-frontend/src/util/numbers.js
+++ basic-testing-frontend/src/util/numbers.js
@@ -1,4 +1,18 @@
-export function transformToNumber(value) {
+export function transformToNumber(input) {
+  /// Convert numbers
+  let value = typeof input === "number" ? `${input}` : input;
+
+  console.log({ value });
+
+  /// Accepts only string numbers
+  if (
+    value === undefined ||
+    value === "" ||
+    typeof value !== "string" ||
+    isNaN(value)
+  ) {
+    throw new Error("Invalid argument");
+  }
+
   return +value;
 }
-
--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -9,6 +9,19 @@ it.todo("should convert a number to a string", () => {
   expect(result).toBe("number");
 });
 
-it.todo("should throw if a argument value is not a number of a string");
+it("should throw if a argument value is not a number or type string number", () => {
+  const num = [];
+  const num2 = "";
+  const num3 = undefined;
+
+  const resultFn = () => transformToNumber(num);
+  const resultFn2 = () => transformToNumber(num2);
+  const resultFn3 = () => transformToNumber(num3);
+
+  expect(resultFn).toThrowError("Invalid argument");
+  expect(resultFn2).toThrowError("Invalid argument");
+  expect(resultFn3).toThrowError("Invalid argument");
+});
+
 it.todo("should throw if an empty argument is provided");
 it.todo("should ignore more than one argument");
