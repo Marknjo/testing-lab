5b3d9aa33bd6a480d256954afd8d75c284806a26
Marknjo <marknjo7@gmail.com>
2022-09-28 14:00:25 +0300

    style(frontend): add todos for testing transformToNumber() function

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,6 @@
+import { it, expect } from "vitest";
+
+it.todo("should convert a number to a string");
+it.todo("should throw if a argument value is not a number of a string");
+it.todo("should throw if an empty argument is provided");
+it.todo("should ignore more than one argument");
