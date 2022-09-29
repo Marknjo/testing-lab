15c601ce08b2d7ee81068f2d7c8cb5c56c754b5d
Marknjo <marknjo7@gmail.com>
2022-09-29 13:55:59 +0300

    test(frontend): preb by adding todos

---
+++ basic-testing-frontend/src/util/validate.test.js
@@ -0,0 +1,13 @@
+import { it, expect, describe } from "vitest";
+
+describe.todo("validateStringNotEmpty() function", () => {
+  it.todo("should throw if supplied string is empty", () => {});
+  it.todo("should be successful if string length is greater than 1", () => {});
+  it.todo("should throw if supplied value is not a string", () => {});
+  it.todo("should be successful if supplied value is a string", () => {});
+});
+
+describe.todo("validateNumber() function", () => {
+  it.todo("should throw if supplied value is not a number", () => {});
+  it.todo("should be successful if supplied value is a number", () => {});
+});
