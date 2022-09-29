054e83f64d9246640ba14f63cfb0e9285e3b83b0
Marknjo <marknjo7@gmail.com>
2022-09-29 14:02:26 +0300

    test(frontend): validateStringNotEmpty() to throw if empty or undefined is supplied

--- basic-testing-frontend/src/util/validate.test.js
+++ basic-testing-frontend/src/util/validate.test.js
@@ -1,9 +1,22 @@
 import { it, expect, describe } from "vitest";
+import { validateStringNotEmpty } from "./validation";
 
-describe.todo("validateStringNotEmpty() function", () => {
-  it.todo("should throw if supplied string is empty", () => {});
+describe("validateStringNotEmpty() function", () => {
+  it("should throw if supplied string is empty", () => {
+    const input1 = "";
+    const input2 = undefined;
+
+    const returnFn1 = () => validateStringNotEmpty(input1);
+    const returnFn2 = () => validateStringNotEmpty(input2);
+
+    expect(returnFn1).toThrowError(/must not be empty/);
+    expect(returnFn2).toThrowError(/must not be empty/);
+  });
+
   it.todo("should be successful if string length is greater than 1", () => {});
+
   it.todo("should throw if supplied value is not a string", () => {});
+
   it.todo("should be successful if supplied value is a string", () => {});
 });
 
