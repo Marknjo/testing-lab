eaad6709dd1cb21da9b5bf6f90e694d449aec8be
Marknjo <marknjo7@gmail.com>
2022-09-30 16:58:22 +0300

    refactor(mocks): writeData() to use mocks

--- mocks-spies/src/util/io.js
+++ mocks-spies/src/util/io.js
@@ -1,7 +1,7 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+import path from "path";
+import fs from "fs/promises";
 
 export default function writeData(data, filename) {
-  const storagePath = path.join(process.cwd(), 'data', filename);
+  const storagePath = path.join(process.cwd(), "data", filename);
   return fs.writeFile(storagePath, data);
 }
