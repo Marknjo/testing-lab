e830d3482cbbc8ca50a36a6d3e60b6a592d16ce7
Marknjo <marknjo7@gmail.com>
2022-10-07 17:13:44 +0300

    test(dom): add test for checking if error message is displayed in th ui

--- dom-testing/util/dom.test.js
+++ dom-testing/util/dom.test.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { it, describe } from "vitest";
+import { it, describe, vi, expect } from "vitest";
 import { Window } from "happy-dom";
 import { showError } from "./dom";
 
@@ -11,8 +11,17 @@ const window = new Window();
 const document = window.document;
 document.write(htmlDocumentContent);
 
+vi.stubGlobal("document", document);
+
 describe("showError()", () => {
-  it("first test", () => {
-    showError("test");
+  it("should add an error paragraph to the id='errors' element", () => {
+    const errMsg = "test";
+
+    showError(errMsg);
+
+    const errorsEl = document.getElementById("errors");
+    const errorParagraph = errorsEl.firstElementChild;
+
+    expect(errorParagraph).not.toBeNull();
   });
 });
